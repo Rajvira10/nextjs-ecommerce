6f032b7c26f5d9aa6ef387854c6c474e43ac5aa0
Rajvira10 <rajvirahmedshuvo10@gmail.com>
2023-05-29 19:15:58 +0600

    added admin authentication

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,12 +3,32 @@ import { MongoDBAdapter } from '@next-auth/mongodb-adapter'
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 
-export default NextAuth({
+const adminEmails = ['rajvir.ahmed.shuvo@g.bracu.ac.bd']
+
+export const authOptions = ({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET
     }),
   ],
-  adapter: MongoDBAdapter(clientPromise)
+  adapter: MongoDBAdapter(clientPromise),
+  callbacks: {
+    session: ({session, token, user}) => {
+      if(adminEmails.includes(session?.user?.email)){
+        return session;
+      } else{
+        return false;
+      }
+    }
+  }
 })
+
+export default NextAuth(authOptions);
+
+export async function isAdminRequest(req,res) {
+  const session = await getServerSession(req,res,authOptions);
+  if (adminEmails.includes(session?.user?.email)){
+    throw 'not an admin';
+  }
+}
